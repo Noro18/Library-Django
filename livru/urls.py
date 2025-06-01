0102a9b622adca24344b2df7b530eb32cc30f471
Noro18 <zeroxg534@gmail.com>
2025-06-01 23:15:50 +0900

    aumenta form edit no mos update nian

--- livru/urls.py
+++ livru/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.empresta_render, name='livru'),
-    path('add', views.add_livru, name='add_livru'),
+    path('add', views.add_livru, name='aumenta_livru'),
+    path('edit/<int:pk>', views.edit_livru, name='edit_livru'),
+    path('delete/<int:prim>', views.delete_livru, name='delete_livru'),
     # path('dashboard/', views.dashboard_render, name='dashboard')
 
 ]
--- livru/views.py
+++ livru/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Livru
 from .forms import LivruForm
 # Create your views here.
@@ -10,11 +10,29 @@ def empresta_render(request):
     })
 
 def add_livru(request):
-    if request.method == 'POST':
-        form = LivruForm(request.POST)
+    if request.method == 'POST': # ida ne'e atu check nia request ne POST ou Get tanba nia form nia url mai ba iha view ida entaun tenki hare nia mehtod se post anta nia save data
+        form = LivruForm(request.POST) # jadi instance husi from maibe nia prense hotu ona ho valor husi post nian 
         if form.is_valid():
             form.save()
             return redirect('livru')
     else:
-        form = LivruForm()
-    return render(request, 'add_livru.html', {'form': form})
+        form = LivruForm() # se quando nai metodo GET ou seidauk iah dados entaun ni kria deit form mamuk ida
+    return render(request, 'add_livru.html', {'form': form}) # e depois maka nia render form ne'e iha add livru no lori hanesan context iha form
+
+def edit_livru(request, pk):
+    livru = Livru.objects.get(pk=pk) # iha ne'e atu fori object sira ho pk ne'ebe define iha tablea
+    if request.method == 'POST':
+        form = LivruForm(request.POST, instance=livru) # ida ne'e nia fill hotu ho valor husi submited form 
+        if form.is_valid():
+            form.save()
+            return redirect('livru')
+    else:
+        form = LivruForm(instance=livru) # iha ne'e nia kria form ida ho inrance ba object ida determina ona
+    return render(request, 'add_livru.html', {'form': form, 'edit': True, 'livru': livru})
+
+def delete_livru(request, prim):
+    # livru = Livru.objects.get(pk=prim) ida bele uza ida ne'e mais diak liu uza get_object_or_404
+
+    livru = get_object_or_404(Livru, pk = prim)
+
+    return render(request, 'delete.html', {'livru_object': livru})
